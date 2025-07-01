35c46ebc4304c45d91eb8eb543ff092d4c2de94b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-01 16:06:34 -0700

    code

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/reiver/space-pod/srv/log"
+)
+
+func main() {
+	log := logsrv.Prefix("main").Begin()
+	defer log.End()
+
+	log.Inform("space-pod ⚡")
+	blur()
+}
